a2276e9fe03dd53ee01cae3ac940c6d62aaa3e85
Cameron Hammel <camhammel@gmail.com>
2018-07-02 19:36:02 -0400

    Added words/places lists and expanded on default texts. Also added a random colour mechanism

--- app/src/main/java/com/cameronhammel/cam/drinkup/Eight.java
+++ app/src/main/java/com/cameronhammel/cam/drinkup/Eight.java
@@ -1,6 +1,7 @@
 package com.cameronhammel.cam.drinkup;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,10 @@ public class Eight extends AppCompatActivity
 {
 
     String [] texts;
+    String [] words;
+    String [] places;
+    int [] colours;
+
     TextView tv;
     ConstraintLayout cl;
 
@@ -24,18 +29,48 @@ public class Eight extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eight);
 
-        texts = new String[10];
+        //names are accessed using the "@" key
+        texts = new String[11];
         texts[0] = "@, list 8 Disney characters or take 3 sips!";
-        texts[1] = "@, take 2 sips!";
+        texts[1] = "@, list 8 European countries or take 3 sips!";
         texts[2] = "@, take a sip each time you're caught swearing in the next 8 turns!";
-        texts[3] = "@, take 3 sips!";
-        texts[4] = "example";
-        texts[5] = "example";
-        texts[6] = "example";
-        texts[7] = "@, example";
-        texts[8] = "@, example";
-        texts[9] = "@, example";
+        texts[3] = "@, draw a # using snapchat and send it to any 8 friends or take 4 sips!";
+        texts[4] = "@, draw a # using snapchat and send it to any 8 friends or take 4 sips!";
+        texts[5] = "@, draw a # using snapchat and send it to any 8 friends or take 4 sips!";
+        texts[6] = "Drink 3 times if you haven't been to & in the past 8 months!";
+        texts[7] = "Drink 3 times if you haven't been to & in the past 8 months!";
+        texts[8] = "Drink 3 times if you haven't been to & in the past 8 months!";
+        texts[9] = "@, send an erotic text of 8 words or more to a friend or take 5 sips!";
+        texts[10] = "@, play the first 8 seconds of a random song on your phone. If nobody can guess the artist, take 3 sips.";
 
+
+        //list of nouns accessed using the # key
+        words = new String[11];
+        words[0] = "giraffe";
+        words[1] = "monkey";
+        words[2] = "penis";
+        words[3] = "superhero";
+        words[4] = "mouse";
+        words[5] = "beer";
+        words[6] = "mustache on another player";
+        words[7] = "winky face";
+        words[8] = "self-portrait";
+        words[9] = "pair of boobs";
+        words[10] = "";
+
+        //list of destinations accessed using the & key
+        places = new String[11];
+        places[0] = "a hospital";
+        places[1] = "the gym";
+        places[2] = "the dentist";
+        places[3] = "a lake or ocean";
+        places[4] = "a beach";
+        places[5] = "a sports arena";
+        places[6] = "bar, pub, or club";
+        places[7] = "a movie theatre";
+        places[8] = "the zoo";
+        places[9] = "an airport";
+        places[10] = "Toronto";
 
         playRound();
     }
@@ -50,15 +85,19 @@ public class Eight extends AppCompatActivity
     @SuppressLint("ResourceAsColor")
     public void refreshText(View view)
     {
-        cl.setBackgroundColor(R.color.colorPrimary);
+        cl.setBackgroundColor((int) (Color.CYAN * Math.random()));
         tv.setText(generateText());
     }
 
     private String generateText()
     {
         String n = texts[(int) (Math.random() * texts.length)];
+        int x = (int) (Math.random() * words.length);
+        int y = (int) (Math.random() * places.length);
         int z = (int) (Math.random() * MainActivity.finalplayers.size());
         n = n.replaceAll("@", MainActivity.finalplayers.get(z) + "");
+        n = n.replaceAll("#", words[x]);
+        n = n.replaceAll("&", places[y]);
         return n;
     }
 }
